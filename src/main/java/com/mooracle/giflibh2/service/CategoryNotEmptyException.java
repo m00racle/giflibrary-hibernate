dd971702a6df69a3ad8ffff9fd1b659bc3fa14bc
mooracle <yanuar.prakosa@gmail.com>
2018-05-12 17:55:22 +0700

    ENTRY 61: Custom Exception in the Service Layer

---
+++ src/main/java/com/mooracle/giflibh2/service/CategoryNotEmptyException.java
@@ -0,0 +1,8 @@
+package com.mooracle.giflibh2.service;
+
+/** ENTRY 61: Custom Exception in the Service Layer
+ * we just need to extends it from super class Exception*/
+
+public class CategoryNotEmptyException extends Exception{
+    //just let it be default from the super class
+}
